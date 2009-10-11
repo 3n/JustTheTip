f54ff3609a66e770593c15e13b5a7e8532ed393b
Ian Collins <ian@powerset.com>
2009-10-11 15:02:38 -0700

    package.yml

--- Source/JustTheTip.js
+++ Source/JustTheTip.js
@@ -1,8 +1,8 @@
 /*=
 name: JustTheTip
 description: Tool-tip class that allows for arbitrary HTML and provides lots of events to hook into.
-@requires core/1.2.3 Class.Extras Element.Event Element.Style Selectors
 @requires more/1.2.3 Class.Binds Element.Position
+@requires core/1.2.3 Class.Extras Element.Event Element.Style Selectors
 @provides JustTheTip
 =*/
 
